a5f08212c6a794a38b2818f7b94f8542836647c6
Kees Krijnen <k.krijnen@home.nl>
2026-01-28 12:54:35 +0100

    Update

--- lib/uart.v
+++ lib/uart.v
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2025, Kees Krijnen.
+ *  Copyright (C) 2025, 2026, Kees Krijnen.
  *
  *  This program is free software: you can redistribute it and/or modify it
  *  under the terms of the GNU Lesser General Public License as published by the
